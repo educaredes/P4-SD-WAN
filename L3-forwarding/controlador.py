d131db5b661c51d8fdd049ea599f7c4e4778293c
Luis Bellido Triana <luis.bellido@upm.es>
2025-12-13 10:37:32 +0100

    Update L2 and L3 examples

--- L3-forwarding/controlador.py
+++ L3-forwarding/controlador.py
@@ -9,7 +9,7 @@ controller = SimpleSwitchP4RuntimeAPI(device_id=1, grpc_port=9559, grpc_ip='172.
 controller.table_set_default('ipv4_lpm','drop')
 controller.table_add('ipv4_lpm', 'ipv4_forward',['10.0.0.1/32'], ['02:fd:00:00:00:01','1'])
 controller.table_add('ipv4_lpm', 'ipv4_forward',['10.1.0.0/24'], ['02:fd:00:00:03:02','2'])
-controller.table_add('ipv4_lpm', 'ipv4_forward',['172.17.2.0/24'], ['02:fd:00:00:02:03','3'])
+#controller.table_add('ipv4_lpm', 'ipv4_forward',['172.17.2.0/24'], ['02:fd:00:00:02:03','3'])
 
 # VERISION CLI
 # table_add MyIngress.ipv4_lpm MyIngress.ipv4_forward 10.0.0.1/32 => 02:fd:00:00:00:01 1
@@ -26,7 +26,7 @@ controller = SimpleSwitchP4RuntimeAPI(device_id=2, grpc_port=9559, grpc_ip='172.
 controller.table_set_default('ipv4_lpm','drop')
 controller.table_add('ipv4_lpm', 'ipv4_forward',['10.1.0.2/32'], ['02:fd:00:00:01:01','1'])
 controller.table_add('ipv4_lpm', 'ipv4_forward',['10.0.0.0/24'], ['02:fd:00:00:02:02','2'])
-controller.table_add('ipv4_lpm', 'ipv4_forward',['172.17.2.0/24'], ['02:fd:00:00:03:03','3'])
+#controller.table_add('ipv4_lpm', 'ipv4_forward',['172.17.2.0/24'], ['02:fd:00:00:03:03','3'])
 
 # GPRC_CLI_RUNTIME
 # table_add MyIngress.ipv4_lpm MyIngress.ipv4_forward 10.1.0.2/32 => 02:fd:00:00:01:01 1
